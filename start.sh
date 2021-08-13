94aa211c3e1866c05be1292432cc09065e8c45f4
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-13 12:18:29 +0530

    I

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 
-pip3 install git+https://github.com/InternetAmethyst/db
+pip3 install -U git+https://github.com/InternetAmethyst/db
 git clone https://github.com/InternetAmethyst/ExtremeProUserbot
 cd ExtremeProUserbot
 python3 -m Extre
